a65705b1e3b8b88541e15011f1d3d7cfaf955ced
Sushmita Chand <sushmitachand@MacBook-Air-3.lan>
2020-08-18 21:30:00 +1200

    Added a score variable to the quiz

--- src/conditionals/ifelse-quizz.py
+++ src/conditionals/ifelse-quizz.py
@@ -1,17 +1,26 @@
+# Implemented a quiz using if/else structures and variables for scoring
+
+score = 0
+
 answer = input("What does CPU stand for?")
 if answer == "central processing unit":
   print("Correct!")
+  score += 1  
 else:
   print("Sorry, wrong answer.")
   
 answer = input("How many bits are in a byte?")
 if answer == "8":
   print("Correct!")
+  score += 1
 else:
   print("Sorry, wrong answer.")
   
 answer = input("Which is bigger: a kilobyte or a megabyte?")
 if answer == "megabyte":
   print("Correct!")
+  score += 1
 else:
   print("Sorry, wrong answer.")
+
+print ("You scored {} points!".format(score))
