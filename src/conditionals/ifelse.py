21b0525431c65f5044e35db82d6deab36653394e
Sushmita Chand <sushmitachand@MacBook-Air-3.lan>
2020-08-18 21:08:51 +1200

    Learning about Conditionals

---
+++ src/conditionals/ifelse.py
@@ -0,0 +1,7 @@
+# This is a selection - making decisions about what should happen based on conditions.
+food = input("What is your favorite food?")
+
+if food == "pizza":
+  print("Yum!")
+else: 
+  print("Yuck")
