5aa3ba43331ef9728b41983c4ed2ce0776eed5a2
Sushmita Chand <sushmitachand@MacBook-Air-3.lan>
2020-08-18 21:17:57 +1200

    Creating a Quizz using if/else structures

---
+++ src/conditionals/ifelse-quizz.py
@@ -0,0 +1,17 @@
+answer = input("What does CPU stand for?")
+if answer == "central processing unit":
+  print("Correct!")
+else:
+  print("Sorry, wrong answer.")
+  
+answer = input("How many bits are in a byte?")
+if answer == "8":
+  print("Correct!")
+else:
+  print("Sorry, wrong answer.")
+  
+answer = input("Which is bigger: a kilobyte or a megabyte?")
+if answer == "megabyte":
+  print("Correct!")
+else:
+  print("Sorry, wrong answer.")
